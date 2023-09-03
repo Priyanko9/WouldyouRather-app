6d6a5d836cc599cf5894455f27ecf6ab574716ed
Priyanko Das <pridas2@WKMZT0210FEC.global.publicisgroupe.net>
2023-09-03 20:05:32 +0530

    adding build files

---
+++ build/precache-manifest.4bc67d67e7261d8780ebd0308f4886a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44fc818264f5715c449ec4417e1b55d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1cab9ed21f090ecadae",
+    "url": "/static/css/main.daee69fb.chunk.css"
+  },
+  {
+    "revision": "1fdaeefc8d3aa61835f6",
+    "url": "/static/js/2.2e245a51.chunk.js"
+  },
+  {
+    "revision": "a1cab9ed21f090ecadae",
+    "url": "/static/js/main.ad65efd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
